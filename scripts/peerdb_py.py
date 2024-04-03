2f0b5c0f2bac863c2f116019d410280e01b7090c
Muhammad Rafi <murafi@cisco.com>
2024-04-03 03:17:11 +0100

    update bgp playbook

--- scripts/peerdb_py.py
+++ scripts/peerdb_py.py
@@ -1,22 +1,29 @@
+from rich import print 
 import requests
 import json
-from rich import print 
 import sys
-
+import os
+requests.packages.urllib3.disable_warnings()
 bgp_asn = sys.argv[1]
-api_key = ''
 
-url = f'https://www.peeringdb.com/api/net?asn={bgp_asn}&key={api_key}'
+# Get the value of the PEERDB_API_KEY environment variable
+api_key = os.environ.get("PEERDB_API_KEY")
 
-response = requests.get(url)
-response.raise_for_status() 
-resp_data = response.json()
+# Check if the PEERDB_API_KEY variable exists
+if api_key is not None:
+    url = f'https://www.peeringdb.com/api/net?asn={bgp_asn}&key={api_key}'
+    response = requests.get(url)
+    response.raise_for_status() 
+    resp_data = response.json()
 
-peer_info = resp_data.get('data', '')[0]
-# print(peer_info)
+    peer_info = resp_data.get('data', '')[0]
+    # print(peer_info)
 
-v4prefixes = peer_info.get('info_prefixes4')
-v6prefixes = peer_info.get('info_prefixes6')
+    v4prefixes = peer_info.get('info_prefixes4')
+    v6prefixes = peer_info.get('info_prefixes6')
 
-print(v4prefixes)
-print(v6prefixes)
+    print(v4prefixes)
+    print(v6prefixes)
+else:
+    print("PeeringDB API Key is not set.")
+
